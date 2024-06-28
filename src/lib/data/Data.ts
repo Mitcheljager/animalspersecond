8186d3112559fdef72ca7e5208cbf8a61f39ca32
Mitchel <mitchel_jager@hotmail.com>
2024-06-28 02:11:40 +0200

    chore: Add more animals

--- src/lib/data/Data.ts
+++ src/lib/data/Data.ts
@@ -2,6 +2,18 @@ import IconChicken from "$lib/images/chicken.svg"
 import IconFish from "$lib/images/fish.svg"
 import IconDuck from "$lib/images/Duck.svg"
 import IconPig from "$lib/images/Pig.svg"
+import IconCow from "$lib/images/Cow.svg"
+import IconSheep from "$lib/images/Sheep.svg"
+import IconRabbit from "$lib/images/Rabbit.svg"
+import IconTurkey from "$lib/images/Turkey.svg"
+import IconGoose from "$lib/images/Goose.svg"
+import IconGoat from "$lib/images/Goat.svg"
+import IconBuffalo from "$lib/images/Buffalo.svg"
+import IconCamel from "$lib/images/Camel.svg"
+import IconPigeon from "$lib/images/Pigeon.svg"
+import IconHorse from "$lib/images/Horse.svg"
+import IconDeer from "$lib/images/Deer.svg"
+import IconKangaroo from "$lib/images/Kangaroo.svg"
 
 export const Data = {
   Chicken: {
@@ -23,5 +35,65 @@ export const Data = {
     name: "Ducks",
     icon: IconDuck,
     annually: 3_000_000_000
-  }
+  },
+  Cow: {
+    name: "Cows",
+    icon: IconCow,
+    annually: 300_000_000
+  },
+  Sheep: {
+    name: "Sheep",
+    icon: IconSheep,
+    annually: 550_000_000
+  },
+  Rabbit: {
+    name: "Rabbits",
+    icon: IconRabbit,
+    annually: 1_000_000_000
+  },
+  Turkey: {
+    name: "Turkeys",
+    icon: IconTurkey,
+    annually: 650_000_000
+  },
+  Goose: {
+    name: "Geese",
+    icon: IconGoose,
+    annually: 700_000_000
+  },
+  Goat: {
+    name: "Goats",
+    icon: IconGoat,
+    annually: 450_000_000
+  },
+  Buffalo: {
+    name: "Buffalo",
+    icon: IconBuffalo,
+    annually: 24_000_000
+  },
+  Camel: {
+    name: "Camels",
+    icon: IconCamel,
+    annually: 3_000_000
+  },
+  Pigeon: {
+    name: "Pigeons (and other birds)",
+    icon: IconPigeon,
+    annually: 45_000_000
+  },
+  Horse: {
+    name: "Horses",
+    icon: IconHorse,
+    annually: 4_500_000
+  },
+  Deer: {
+    name: "Deer",
+    icon: IconDeer,
+    annually: 10_000_000
+  },
+  Kangaroo: {
+    name: "Kangaroos",
+    icon: IconKangaroo,
+    annually: 3_000_000
+  },
 }
