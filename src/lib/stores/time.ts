ec7027b5b345b95aa9c265695f97159397f39838
Mitchel <mitchel_jager@hotmail.com>
2024-07-01 15:07:37 +0200

    refactor: Start timer from 0

--- src/lib/stores/time.ts
+++ src/lib/stores/time.ts
@@ -6,7 +6,7 @@ export const secondsSinceArrival: Readable<number> = createIntervalStore()
 function createIntervalStore(): Readable<number> {
 	const initial: Date = new Date()
 
-  return readable(1, set => {
+  return readable(0, set => {
     const update = () => set((+new Date() - +initial) / 1000)
 
     const interval = setInterval(update, 1000)
