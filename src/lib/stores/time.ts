a9818e7d1625a69e2d758de6c44ef95a9d72e744
Mitchel <mitchel_jager@hotmail.com>
2024-07-01 20:09:32 +0200

    feat: Start time 1 second ahead

--- src/lib/stores/time.ts
+++ src/lib/stores/time.ts
@@ -6,8 +6,8 @@ export const secondsSinceArrival: Readable<number> = createIntervalStore()
 function createIntervalStore(): Readable<number> {
 	const initial: Date = new Date()
 
-  return readable(0, set => {
-    const update = () => set((+new Date() - +initial) / 1000)
+  return readable(1, set => {
+    const update = () => set((+new Date() + 1000 - +initial) / 1000)
 
     const interval = setInterval(update, 1000)
 
