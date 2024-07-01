3bdbab230ab4f7d13d5f3b011315b9e81d5b14b6
Mitchel <mitchel_jager@hotmail.com>
2024-07-01 20:51:41 +0200

    style: Show less intense shadow on mobile
    The render of the shadow is a bit too heavy on mobile

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -27,6 +27,17 @@
   header {
 		margin: 0 0 $layout-offset;
 		color: $yellow;
+		filter: drop-shadow(-1.5rem 15px 10px rgba($black, 0.3));
+
+		@media (min-width: 768px) {
+			// filter:
+			// 	drop-shadow(-0.5rem 3px 2px rgba($black, 0.1))
+			// 	drop-shadow(-1rem 10px 5px rgba($black, 0.1))
+			// 	drop-shadow(-1.5rem 15px 10px rgba($black, 0.1))
+			// 	drop-shadow(-2rem 25px 15px rgba($black, 0.2))
+			// 	drop-shadow(-2.5rem 40px 30px rgba($black, 0.2))
+			// 	drop-shadow(-3rem 100px 75px rgba($black, 0.3));
+		}
 	}
 
 	h1 {
