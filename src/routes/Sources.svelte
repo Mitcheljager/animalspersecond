4d1641a7c746b2a22829493d6ec3e9e7b3780f07
Mitchel <mitchel_jager@hotmail.com>
2024-11-18 21:10:09 +0100

    chore: Typo

--- src/routes/Sources.svelte
+++ src/routes/Sources.svelte
@@ -6,7 +6,7 @@
 
 <h4>Sources</h4>
 
-<p><em>Various sources were used for each animal. When numbers differed the average was taken, depending on which most sourced seemed to agree on. When ranges were given the lower end of the range was used.</em></p>
+<p><em>Various sources were used for each animal. When numbers differed the average was taken, depending on which most sources seemed to agree on. When ranges were given the lower end of the range was used.</em></p>
 
 {#each animals as { name, sources }}
   <p>
